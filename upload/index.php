9a4adc8784c75949eddfda0ac4f7061c437f17e9
roudyirany <roudyirany@me.com>
2017-07-13 16:53:57 +0300

    edit

--- upload/index.php
+++ upload/index.php
@@ -15,7 +15,7 @@
     $file_ext = explode('.', $file_name);
     $file_ext = strtolower(end($file_ext));
 
-    $allowed = array('png', 'jpg', 'jpeg');
+    $allowed = array('png', 'jpg', 'jpeg','gif');
 
     //filename
     $id = 'uploads/Test';
@@ -26,18 +26,12 @@
     }
 
     if (in_array($file_ext, $allowed)) {
-        if ($file_error === 0) {
-            if ($file_size <= 10485760) {
-
                 $file_name_new = uniqid('', true) . '.' . $file_ext;
                 $file_destination = $id .'/'. $file_name_new;
 
                 if (move_uploaded_file($file_tmp, $file_destination)) {
                     echo $file_destination;
                 }
-            }
-        }
     }
-}
 
     ?>
